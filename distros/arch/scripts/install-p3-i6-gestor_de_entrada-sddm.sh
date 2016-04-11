3cae341bc15f99bea96f0b441fa2b1ece883a1ca
seilaeu <ribeiro.jose.a.n@gmail.com>
2016-04-11 17:09:26 +0100

    Update install-p3-i6-gestor_de_entrada-sddm.sh

--- distros/arch/scripts/install-p3-i6-gestor_de_entrada-sddm.sh
+++ distros/arch/scripts/install-p3-i6-gestor_de_entrada-sddm.sh
@@ -1,10 +1,3 @@
-# Gestor de entrada no sistema
-
 ### SDDM
-#pacman -S sddm &&
-#systemctl enable sddm
-
-### LIGHTDM
-pacman -S lightdm lightdm-gtk-greeter numlockx &&
-systemctl enable lightdm
-systemctl start lightdm
+pacman -S sddm &&
+systemctl enable sddm
