82fef28f0b28414ca1417c6a33860d2c5f71508b
seilaeu <ribeiro.jose.a.n@gmail.com>
2016-04-16 18:33:51 +0100

    Update install-p3-i3.1-xfce.sh

--- distros/arch/scripts/install-p3-i3.1-xfce.sh
+++ distros/arch/scripts/install-p3-i3.1-xfce.sh
@@ -1,1 +1,1 @@
-pacman -S xfce4 xfce4-whiskermenu-plugin xfce4-taskmanager thunar-archive-plugin xame leafpad gvfs gparted 
+pacman -S xfce4 xfce4-whiskermenu-plugin xfce4-taskmanager thunar-archive-plugin xame leafpad epdfview  gvfs gparted 
