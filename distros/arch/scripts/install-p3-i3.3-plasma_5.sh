365f6b4ea1af754b92766c3154c02a53821c18b0
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-01-05 18:53:24 +0000

    Update install-p3-i3.3-plasma_5.sh

--- distros/arch/scripts/install-p3-i3.3-plasma_5.sh
+++ distros/arch/scripts/install-p3-i3.3-plasma_5.sh
@@ -1,3 +1,4 @@
 # Plasma 5
 
-pacman -S plasma kmymoney kcalc k3b okular digikam gwenview qbittorrent ark sddm konsole kwrite filelight clementine kde-l10n-pt kdeconnect spectacle
+pacman -S plasma kmymoney kcalc k3b okular digikam gwenview qbittorrent ark sddm konsole kwrite \
+filelight clementine kde-l10n-pt kdeconnect spectacle kfind
