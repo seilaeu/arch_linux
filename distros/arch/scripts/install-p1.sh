77e9b1ff566942e795eb71b2a8a6401ac35f25bc
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-03-21 23:42:42 +0000

    Update install-p1.sh

--- distros/arch/scripts/install-p1.sh
+++ distros/arch/scripts/install-p1.sh
@@ -1,61 +1,4 @@
 #/bin/bash
-# Teclado
-
-loadkeys pt-latin9 &&
-
-# Formatar partição raiz
-
-################################################
-#                Importante!!!                 #              # 
-#  Mudar /dev/pclvm/arch? para a partição alvo #
-################################################
-#     | |
-#     v v
-
-
-
-# BTRFS
-
-mkfs.btrfs -f -L "arch" /dev/pclvm/arch? &&
-
-# Criar directorias (activo e reserva) e subvolume raiz
-
-mkdir -p /media/arch &&
-
-##Mudar sdaX para a partição alvo
-
-mount -o rw,suid,dev,exec,auto,nouser,async,noatime,autodefrag,compress=lzo /dev/pclvm/arch? /media/arch &&
-mkdir -p /media/arch/__reserva &&
-mkdir -p /media/arch/__activo &&
-btrfs sub create /media/arch/__activo/raiz &&
-umount -l /media/arch &&
-sleep 7 &&
-
-################################################
-# Importante!!! Mudar sdaX para a partição alvo#
-################################################
-
-# EXT4
-
-#mkfs.ext4 /dev/sdaX &&
-#e2label /dev/sdaX arch &&
-
-#blkid &&
-
-# Montar raiz em /mnt
-
-################################################
-# Importante!!! Mudar sdaX para a partição alvo#
-################################################
-
-if [[ ! -e /mnt ]]; then 
-    mkdir /mnt
-elif [[ ! -d /mnt ]]; then
-    echo "/mnt already exists but is not a directory" 1>&2
-fi
-mount /dev/sdaX /mnt
-df -h &&
-sleep 7 &&
 
 # Sites espelho
 #echo "descomentar servidores portugueses em /etc/pacman.d/mirrorlist" &&
@@ -70,7 +13,7 @@ sleep 7 &&
 # Nota: Portugal, infelizmente, nao tem sitios-espelho seguros
 
 pacman -Sy reflector &&
-reflector --verbose --country 'Germany' -l 200 -p https --sort rate --save /etc/pacman.d/mirrorlist &&
+reflector --verbose --country 'Portugal' --sort rate --save /etc/pacman.d/mirrorlist &&
 
 # Instalar sistema base
 pacstrap -i /mnt/ base base-devel &&
