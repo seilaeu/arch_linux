56707c0b90b83e0d16eec9d1db4e4d370469f5fc
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-03-21 23:43:01 +0000

    Update install-p1.sh

--- distros/arch/scripts/install-p1.sh
+++ distros/arch/scripts/install-p1.sh
@@ -1,16 +1,6 @@
 #/bin/bash
 
 # Sites espelho
-#echo "descomentar servidores portugueses em /etc/pacman.d/mirrorlist" &&
-
-#echo 'Server = http://archlinux.dcc.fc.up.pt/$repo/os/$arch' > /etc/pacman.d/mirrorlist &&
-#echo 'Server = http://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist &&
-#echo 'Server = http://ftp5.gwdg.de/pub/linux/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist &&
-#echo 'Server = http://mirror.bitjungle.info/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist &&
-#echo 'Server = http://archlinux.cu.be/$repo/os/$arch' >> /etc/pacman.d/mirrorlist &&
-
-# Usar o reflector para gerar sitios-espelho alemaes seguros, ordenados por velocidade de transferencia decrescente:
-# Nota: Portugal, infelizmente, nao tem sitios-espelho seguros
 
 pacman -Sy reflector &&
 reflector --verbose --country 'Portugal' --sort rate --save /etc/pacman.d/mirrorlist &&
