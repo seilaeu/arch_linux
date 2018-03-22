7c10a49289e82b3fe2cfe5a9ab4930d5b5659883
seilaeu <ribeiro.jose.a.n@gmail.com>
2018-03-22 01:01:20 +0000

    Update versões_programas_essenciais.sh

--- scripts/versões_programas_essenciais.sh
+++ scripts/versões_programas_essenciais.sh
@@ -3,8 +3,6 @@ google-chrome-stable --version
 firefox --version
 libreoffice --version | grep LibreOffice
 kmymoney --version |grep KMyMoney
-audacious --version
-amarok --version |grep Amarok
 vlc --version |grep "VLC media"
 k3b --version |grep K3b
 
