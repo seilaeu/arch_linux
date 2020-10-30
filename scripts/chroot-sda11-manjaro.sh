78ea1058cb9efacd0a6217ebe15a2ad193564ec5
seilaeu <joseribeiro@protonmail.com>
2020-10-30 03:26:40 +0000

    Delete chroot-sda11-manjaro.sh

--- scripts/chroot-sda11-manjaro.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-if [ ! -d /media/manjaro ]; 
-then sudo mkdir /media/manjaro
-fi
-sudo mount /dev/sda11 /media/manjaro
-sudo mount -t proc none /media/manjaro/proc
-sudo mount --rbind /dev /media/manjaro/dev
-sudo mount --rbind /sys /media/manjaro/sys
-sudo chroot /media/manjaro/ 
