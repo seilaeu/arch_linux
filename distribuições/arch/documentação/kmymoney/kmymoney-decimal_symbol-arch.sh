864a6d9ec3b39405b563403298b126eab4367513
seilaeu <joseribeiro@protonmail.com>
2022-12-05 22:04:38 +0000

    Delete distribuições/arch/documentação/kmymoney directory

--- distribuições/arch/documentação/kmymoney/kmymoney-decimal_symbol-arch.sh
+++
@@ -1,9 +0,0 @@
-cat << EOF >> $HOME/.kde4/share/config/kdeglobals
-
-[Locale]
-DecimalSymbol=,
-MonetaryDecimalSymbol=,
-MonetaryThousandsSeparator=.
-ThousandsSeparator=.
-
-EOF
--- distribuições/arch/documentação/kmymoney/kmymoney-decimal_symbol-debian-fedora.sh
+++
@@ -1,9 +0,0 @@
-cat << EOF >> $HOME/.kde/share/config/kdeglobals
-
-[Locale]
-DecimalSymbol=,
-MonetaryDecimalSymbol=,
-MonetaryThousandsSeparator=.
-ThousandsSeparator=.
-
-EOF
