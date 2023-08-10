dc65f7ca3e912a206ca4c950643e896c17dc54bd
seilaeu <joseribeiro@protonmail.com>
2023-08-10 19:18:39 +0100

    updating file

---
+++ scripts/activar-ipv6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+echo
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
+echo
+ifconfig -a | grep inet6
+echo
---
+++ scripts/desactivar-ipv6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+echo
