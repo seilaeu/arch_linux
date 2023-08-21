f0950c4f699296308ed8a3fc84a71c1bcb25b301
seilaeu <joseribeiro@protonmail.com>
2023-08-21 17:34:15 +0100

    updating file

--- scripts/limpar-sistema-slackware.sh
+++ scripts/limpar-sistema-slackware.sh
@@ -5,23 +5,11 @@ echo
 rm -rf /home/$USER/.cache/* 
 mkdir /home/$USER/.cache/thumbnails/
 rm -rf /home/$USER/.local/share/Trash/* 
-sudo rm -rf /mnt/dados/.Trash-*
+sudo rm -rf /media/dados/.Trash-*
 sudo rm -rf /root/.cache/*
 sudo rm -rf /usr/tmp/* 
 
 echo 
-
-echo "Limpar Pacman Cache"
-sudo pacman -Scc
-yay -Scc
-
-echo
-
-echo "Limpar Orfãos"
-sudo pacman -Rcsn $(pacman -Qqtd)
-yay -Yc
-
-echo
 
 echo "Sistema limpo!"
 
