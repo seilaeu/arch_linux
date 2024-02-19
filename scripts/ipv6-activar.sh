09c574d6b349a300d07252301c09cca07a603ecb
seilaeu <joseribeiro@protonmail.com>
2024-02-19 18:20:54 +0000

    updating file

--- scripts/ipv6-activar.sh
+++ scripts/ipv6-activar.sh
@@ -3,6 +3,7 @@
 echo
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=0
 echo
 ifconfig -a | grep inet6
 echo
